a55c55aa0681948bd62dcdd1f4d488a43c573351
Rohit Agarwal <programmingwithrohit@gmail.com>
2021-05-23 16:18:44 +0530

    Added LCS for two strings

---
+++ src/main/java/dp/LongestCommonSubsequenceTwoStrings.java
@@ -0,0 +1,66 @@
+package dp;
+
+import java.util.Arrays;
+
+/**
+ * @author Rohit Agarwal on 20/05/21 11:54 am
+ * @copyright www.javamultiplex.com
+ */
+public class LongestCommonSubsequenceTwoStrings {
+
+
+    public static int lcsTD(String s1, String s2, int i, int j, int n, int m, int[][] dp) {
+        if (i == n || j == m) {
+            return 0;
+        }
+
+        if (dp[i][j] != -1) {
+            return dp[i][j];
+        }
+
+        if (s1.charAt(i) == s2.charAt(j)) {
+            dp[i][j] = 1 + lcsTD(s1, s2, i + 1, j + 1, n, m, dp);
+        } else {
+            dp[i][j] = Math.max(lcsTD(s1, s2, i + 1, j, n, m, dp), lcsTD(s1, s2, i, j + 1, n, m, dp));
+        }
+        return dp[i][j];
+    }
+
+    public static int lcsBU(String s1, String s2) {
+        int n = s1.length();
+        int m = s2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for (int i = 0; i <= n; i++) {
+            Arrays.fill(dp[i], 0);
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s1.charAt(i - 1) == s2.charAt(j - 1)) {
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+
+    public static void main(String[] args) {
+
+        String s1 = "striver";
+        String s2 = "raj";
+
+        int n = s1.length();
+        int m = s2.length();
+
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        System.out.println(lcsTD(s1, s2, 0, 0, n, m, dp));
+
+        System.out.println(lcsBU(s1, s2));
+    }
+}
